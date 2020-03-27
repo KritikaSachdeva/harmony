3621a3694f82e235ef723fd223f522879f75a871
Kritika Sachdeva <krisachdeva.02@gmail.com>
2020-03-28 03:13:03 +0530

    setting up an initial MVC application

---
+++ harmony/harmony/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using harmony.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace harmony.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            return View(movie);
+        }
+    }
+}
