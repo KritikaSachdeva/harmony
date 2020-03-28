c0e12481d7fab9c01767d1e5bf7e0233fe75c2f9
Kritika Sachdeva <krisachdeva.02@gmail.com>
2020-03-29 00:04:40 +0530

    custom attribute routing

--- harmony/harmony/Controllers/MoviesController.cs
+++ harmony/harmony/Controllers/MoviesController.cs
@@ -35,7 +35,13 @@ namespace harmony.Controllers
                 sortBy = "Name";
 
             return Content(String.Format("pageIndex={0} & sortBy={1}", pageIndex, sortBy));
-
         }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
     }
 }
