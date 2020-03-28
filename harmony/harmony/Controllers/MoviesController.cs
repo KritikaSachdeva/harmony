ddfbdcac336986169ac7a3a33855829800064c68
Kritika Sachdeva <krisachdeva.02@gmail.com>
2020-03-28 21:45:28 +0530

    understanding action results and action parameters

--- harmony/harmony/Controllers/MoviesController.cs
+++ harmony/harmony/Controllers/MoviesController.cs
@@ -13,7 +13,29 @@ namespace harmony.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
-            return View(movie);
+            //return View(movie);
+            //return Content("Hello world");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page="1", sortBy="name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id from request =" + id);
+        }
+
+        // GET: Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0} & sortBy={1}", pageIndex, sortBy));
+
         }
     }
 }
