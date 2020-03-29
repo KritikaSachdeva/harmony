b7d3905670acff0df1219b6bef766862d95a59a7
Kritika Sachdeva <krisachdeva.02@gmail.com>
2020-03-29 18:11:33 +0530

    completed second module -- view model and partial views

--- harmony/harmony/Controllers/MoviesController.cs
+++ harmony/harmony/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using harmony.Models;
+using harmony.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,32 +10,45 @@ namespace harmony.Controllers
 {
     public class MoviesController : Controller
     {
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
-            //return View(movie);
-            //return Content("Hello world");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page="1", sortBy="name"});
+            var customers = new List<Customer>
+            {
+                new Customer {Name= "Kritika"},
+                new Customer {Name= "Vishal"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
         }
 
         public ActionResult Edit(int id)
         {
             return Content("Id from request =" + id);
-        }
-
-        // GET: Movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0} & sortBy={1}", pageIndex, sortBy));
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
---
+++ harmony/harmony/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using harmony.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace harmony.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
