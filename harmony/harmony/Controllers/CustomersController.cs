546f6559060e66f101bbaf05958ace983708e2bf
Kritika Sachdeva <krisachdeva.02@gmail.com>
2020-03-30 01:15:41 +0530

    module 3- soln1 --added membership type to customers' list

--- harmony/harmony/Controllers/CustomersController.cs
+++ harmony/harmony/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using harmony.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,30 +11,33 @@ namespace harmony.Controllers
 {
     public class CustomersController : Controller
     {
+        private MyDBContext _context;
+
+        public CustomersController()
+        {
+            _context = new MyDBContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
         }
-         
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
-        }
-
+      
     }
 }
