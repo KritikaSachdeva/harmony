7ca4a8152d08292f9e5e806297963870546265f1
Kritika Sachdeva <krisachdeva.02@gmail.com>
2020-03-30 16:16:04 +0530

    module 4 -- added create/update/edit forms for customer

--- harmony/harmony/Models/Customer.cs
+++ harmony/harmony/Models/Customer.cs
@@ -9,6 +9,7 @@ namespace harmony.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
@@ -16,7 +17,11 @@ namespace harmony.Models
         public bool IsSubscribedToNewsLetter { get; set; }
 
         public MembershipType MembershipType { get; set; }
+        
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
+
+        [Display(Name= "Date of Birth")]
         public DateTime? Birthdate { get; set; }
     }
 }
--- harmony/harmony/Models/MyDBContext.cs
+++ harmony/harmony/Models/MyDBContext.cs
@@ -14,5 +14,6 @@ namespace harmony.Models
         }
         public DbSet<Customer> Customers { get; set; } // My domain models
         public DbSet<Movie> Movies { get; set; }// My domain models
+        public DbSet<MembershipType> MembershipTypes { get; set; }
     }
 }
