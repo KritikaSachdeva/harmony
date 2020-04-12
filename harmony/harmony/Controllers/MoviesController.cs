b073a27a96117e3b4861da67a349a50b1e4288f5
Kritika Sachdeva <krisachdeva.02@gmail.com>
2020-04-12 22:02:17 +0530

    module 5- soln --added validations in movie forms

--- harmony/harmony/Controllers/MoviesController.cs
+++ harmony/harmony/Controllers/MoviesController.cs
@@ -36,8 +36,19 @@ namespace harmony.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+                };
+
+                return View("MovieForm", viewModel);
+            }
+
             if (movie.Id == 0)
             {
                 movie.DateAdded = DateTime.Now;
@@ -64,9 +75,8 @@ namespace harmony.Controllers
             if (movie == null)
                 return HttpNotFound();
 
-            var viewModel = new MovieFormViewModel
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
                 Genres = _context.Genres.ToList()
             };
 
--- harmony/harmony/Models/Movie.cs
+++ harmony/harmony/Models/Movie.cs
@@ -27,6 +27,7 @@ namespace harmony.Models
         public DateTime ReleaseDate { get; set; }
 
         [Display(Name = "Number in Stock")]
+        [Range(1, 20)]
         public byte NumberInStock { get; set; }
     }
 }
