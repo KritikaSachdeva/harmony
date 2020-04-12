290540b6d08baf7f38cb647932af8facd0d35752
Kritika Sachdeva <krisachdeva.02@gmail.com>
2020-04-13 00:07:49 +0530

    module 6 -- building restful services with web APIs

---
+++ harmony/harmony/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using harmony.Dtos;
+using harmony.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace harmony.App_Start
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
